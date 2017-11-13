02ce1458aaafe25c41c46c26feb47269ba95ed8e
Cameron McEvenue <mcevenc@gmail.com>
2017-11-13 17:36:10 -0500

    Added ExtractTextPlugin to compile sass to css file in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSCSS = new ExtractTextPlugin({
+	filename: "css/main.css",
+	//disabled: false,
+	allChunks: true
+});
 
 const config = {
 	entry: './src/js/main.js',
+
 	output: {
 		path: path.resolve(__dirname, 'dist/'),
 		filename: 'js/main.bundle.js'
 	},
+
 	module: {
 		rules: [
-			{test: /\.css$/, use: ['style-loader','css-loader','sass-loader']}
+
+			{
+				test: /\.scss$/, 
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader','sass-loader']
+				})
+			}
+
 		]
 	},
+
 	plugins: [
+
 		new HtmlWebpackPlugin({
 			template: './src/index.html', // src html doc to compile from
 			hash: true // add a hash onto the script tags generated from plugin
-		})
+		}),
+
+		extractSCSS
+		
 	]
 }
 
